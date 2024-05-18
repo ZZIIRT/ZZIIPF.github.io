541e86b87f842ff7bb3eabd1dbaeff28b7308c5d
ZZII <149934740+ZZIIRT@users.noreply.github.com>
2024-05-18 22:19:22 +0300

    New Site
    1

---
+++ tools/js/blocked/button.js
@@ -0,0 +1,6 @@
+    window.addEventListener('keydown', function(event) {
+        if (event.ctrlKey && (event.key === 's' || event.key === 'S')) {
+            event.preventDefault();
+            window.location.href = 'https://www.youtube.com/watch?v=FDN-9148FPs';
+        }
+    });
